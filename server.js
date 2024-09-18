aa8ea56fa0111153238ff35dfe10fad6fe0b974c
Dien973 <dientran7890@gmail.com>
2024-09-18 08:40:25 +0700

    update student info

---
+++ server.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const app = require('./src/app');
+
+//start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`server is running on port ${port}.`);
+});
