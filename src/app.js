8835d4540c6749e98ad4df3f2eb789f29d800e30
Dien Tran <dientran7890@gmail.com>
2024-10-26 13:05:11 +0700

    Standardize JSON reponses

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+
+const JSend = require('./jsend');
 const contactsRouter = require('./routes/contacts.router');
 
 const app = express();
@@ -9,10 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    return res.json({
-        message: 'ok'
-    });
+    return res.json(JSend.success());
 });
+
 contactsRouter.setup(app);
 
 module.exports = app;
